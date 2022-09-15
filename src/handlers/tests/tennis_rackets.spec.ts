ce43b5ddff3e6c8665911af47e157ec2f2adb2f7
Gabor Nemeth <97887512+Gane02x@users.noreply.github.com>
2022-09-15 10:30:25 +0200

    Update tennis_rackets.spec.ts

--- src/handlers/tests/tennis_rackets.spec.ts
+++ src/handlers/tests/tennis_rackets.spec.ts
@@ -3,11 +3,10 @@ import app from "../../server";
 import { Server } from "http";
 
 let server: Server;
-
 describe("TennisRacket Handler", () => {
   server = app.listen();
 
-  it("should return an error when calling GET /rackets", async () => {
+  it("should return a list of rackets when calling GET /rackets", async () => {
     const response = await request(server).get("/rackets");
     expect(response.status).toBe(200);
   });
@@ -15,14 +14,14 @@ describe("TennisRacket Handler", () => {
     const response = await request(server).get("/rackets/1");
     expect(response.status).toBe(200);
   });
-  it("should return an error when calling POST /rackets without token", async () => {
+  it("should create a racket when calling POST /rackets", async () => {
     const response = await request(server).post("/rackets").send({
       firstname: "example",
       lastname: "name",
       username: "exampleUser",
       password: "examplePW",
     });
-    expect(response.statusCode).toBe(401);
+    expect(response.status).toBe(200);
   });
   server.close();
 });
