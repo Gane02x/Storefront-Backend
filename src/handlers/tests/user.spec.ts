84593089691a8bbca7be57974bb5e1801c42e212
Gabor Nemeth <97887512+Gane02x@users.noreply.github.com>
2022-09-15 10:30:40 +0200

    Update user.spec.ts

--- src/handlers/tests/user.spec.ts
+++ src/handlers/tests/user.spec.ts
@@ -3,17 +3,16 @@ import app from "../../server";
 import { Server } from "http";
 
 let server: Server;
-
 describe("User Handler", () => {
   server = app.listen();
 
-  it("should return an error when calling GET /users without token", async () => {
+  it("should return a list of users when calling GET /users", async () => {
     const response = await request(server).get("/users");
-    expect(response.status).toBe(401);
+    expect(response.status).toBe(200);
   });
-  it("should return an error when calling GET /users/:id without token", async () => {
+  it("should return a specific user when calling GET /users/:id", async () => {
     const response = await request(server).get("/users/1");
-    expect(response.status).toBe(401);
+    expect(response.status).toBe(200);
   });
   it("should create a user when calling POST /users", async () => {
     const response = await request(server).post("/users").send({
@@ -22,7 +21,7 @@ describe("User Handler", () => {
       username: "exampleUser",
       password: "examplePW",
     });
-    expect(response.statusCode).toBe(200);
+    expect(response.status).toBe(200);
   });
   server.close();
 });
