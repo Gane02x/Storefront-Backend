8e0ee5733d3fcb76184ea543828d0fe8409e595d
Gabor Nemeth <97887512+Gane02x@users.noreply.github.com>
2022-09-15 10:22:26 +0200

    Update order.spec.ts

--- src/handlers/tests/order.spec.ts
+++ src/handlers/tests/order.spec.ts
@@ -7,28 +7,28 @@ let server: Server;
 describe("Order Handler", () => {
   server = app.listen();
 
-  it("should return an error when calling GET /orders without token", async () => {
+  it("should a list of orders when calling GET /orders", async () => {
     const response = await request(server).get("/orders");
-    expect(response.status).toBe(401);
+    expect(response.status).toBe(200);
   });
-  it("should return a error when calling GET /orders/:id without token", async () => {
+  it("should a specific order when calling GET /orders/:id", async () => {
     const response = await request(server).get("/orders/1");
-    expect(response.status).toBe(401);
+    expect(response.status).toBe(200);
   });
-  it("should return an error when calling POST /orders without token", async () => {
+  it("should create an order when calling POST /orders", async () => {
     const response = await request(server).post("/orders").send({
       status: "active",
       user_id: 1,
     });
-    expect(response.statusCode).toBe(401);
+    expect(response.statusCode).toBe(200);
   });
-  it("should return an error when calling POST /orders/:id/products without token", async () => {
+  it("should add products to a specific order when calling POST /orders/:id/products", async () => {
     const response = await request(server).post("/orders/1/products").send({
       quantity: 10,
       order_id: 1,
       racket_id: 1,
     });
-    expect(response.status).toBe(401);
+    expect(response.status).toBe(200);
   });
   server.close();
 });
